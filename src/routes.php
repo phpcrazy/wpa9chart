3f218e4f8e2b351dd01bce3f46350e6b5ebf8334
myanmarlinks <myanmarlinks.net@gmail.com>
2013-09-28 12:25:10 +0630

    Refine Routes

--- src/routes.php
+++ src/routes.php
@@ -4,17 +4,6 @@ $app->get('barget2013', '/bar/get/{year}', array('year' => '2013'));
 $app->get('hello', '/hello');
 $app->get('bye', '/bye');
 */
-/*
-https://github.com/phpcrazy/wpa9chart
-composer
-route
-Config Loader
-Dependency Injection
-Route Resolving
-Controller Resolving
-Model
-AngularJS
-*/
 
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
